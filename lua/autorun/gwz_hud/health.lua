aaaf6d687214579b220e0a7b3175af62b7fced62
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2021-06-05 11:19:53 +0300

    Armorbar

--- lua/autorun/gwz_hud/health.lua
+++ lua/autorun/gwz_hud/health.lua
@@ -18,7 +18,7 @@ if CLIENT then
             font = "Open Sans Light",
             extended = false,
             shadow = true,	
-            size = multiplier * 20,
+            size = multiplier * 18,
             antialias = true,
         } )
     end
@@ -36,6 +36,7 @@ if CLIENT then
     -- Textures
     local bgGradient = Material( "hud/bg_gradient.png" )
     local iconServerAdmin = Material( "hud/icon_serveradmin.png" )
+    local bgArmorGrain = Material( "hud/bg_armor_grain.png", "noclamp")
 
     local rect_darkbg = Color(61, 58, 56, 155)
     local rect_lightbg = Color(197, 200, 191)
@@ -44,8 +45,12 @@ if CLIENT then
     -- By default set is as false
     hud_isDisplayed = false
     background_alpha = 0
+    dark_background_alpha = 0
+    light_background_alpha = 0
     text_alpha = 0
     lowhp_alpha = 0
+
+    allow_armorbreak_effect = false
 
     lenght = 169
 
@@ -59,12 +64,18 @@ if CLIENT then
             start = SysTime()
             background_alpha = Lerp(FrameTime() * 30, background_alpha, 255)
             text_alpha = Lerp(FrameTime() * 30, text_alpha, 255)
+            dark_background_alpha = Lerp(FrameTime() * 30, dark_background_alpha, 155)
+            light_background_alpha = Lerp(FrameTime() * 30, light_background_alpha, 191)
         end
 
         if !pPlayer:Alive() then
             start = SysTime()
             background_alpha = Lerp(FrameTime() * 50, background_alpha, 0)       
             text_alpha = Lerp(FrameTime() * 50, text_alpha, 0)
+            dark_background_alpha = Lerp(FrameTime() * 100, dark_background_alpha, 0)
+            light_background_alpha = Lerp(FrameTime() * 50, light_background_alpha, 0)
+
+            allow_armorbreak_effect = false;
         end
 
         hud_offset = math.Clamp(GetConVar("gwz_hud_offset_from_corner"):GetInt(), 0, 100)
@@ -82,14 +93,14 @@ if CLIENT then
         -- Draw user's nickname
         surface.SetFont( "GWZ_SmallNickname" )
         surface.SetTextColor( playerColor.r, playerColor.g, playerColor.b, text_alpha )
-        surface.SetTextPos( ( 70 + hud_offset) * hud_scale, ScrH() - 98 * hud_scale - (hud_offset / 2)) 
+        surface.SetTextPos( ( 61 + hud_offset) * hud_scale, ScrH() - 98 * hud_scale - (hud_offset / 2)) 
         surface.DrawText( pPlayer:GetName() )
 
         -- Draw a star if user is a superadmin
         if pPlayer:IsSuperAdmin() then
             surface.SetDrawColor( playerColor.r, playerColor.g, playerColor.b, text_alpha )
             surface.SetMaterial( iconServerAdmin )
-            surface.DrawTexturedRect( (45 + hud_offset) * hud_scale, ScrH() - 96 * hud_scale - (hud_offset / 2), 16 * hud_scale, 16 * hud_scale)
+            surface.DrawTexturedRect( (40 + hud_offset) * hud_scale, ScrH() - 98 * hud_scale - (hud_offset / 2), 16 * hud_scale, 16 * hud_scale)
         end
 
         local function sinlerp(t, from, to)
@@ -98,20 +109,55 @@ if CLIENT then
 
         --- Draw healthbar
         -- Background
-        surface.SetDrawColor( rect_darkbg )
-        surface.DrawRect((70 + hud_offset) * hud_scale, ScrH() - 63 * hud_scale - (hud_offset / 2), 171 * hud_scale, 8 * hud_scale)
+        surface.SetDrawColor( rect_darkbg.r, rect_darkbg.g, rect_darkbg.b, dark_background_alpha )
+        surface.DrawRect((64 + hud_offset) * hud_scale, ScrH() - 65 * hud_scale - (hud_offset / 2), 197 * hud_scale, 8 * hud_scale)
 
         -- Foreground red
-        surface.SetDrawColor( rect_redbg )
+        surface.SetDrawColor( rect_redbg.r, rect_redbg.g, rect_redbg.b, dark_background_alpha )
         start = SysTime()
-        lenght = Lerp(FrameTime() * 2, lenght, pPlayer:Health() * 1.69)
-        surface.DrawRect((71 + hud_offset) * hud_scale, ScrH() - 62 * hud_scale - (hud_offset / 2), lenght * hud_scale, 6 * hud_scale)
+        lenght = Lerp(FrameTime() * 2, lenght, pPlayer:Health() * 1.95)
+        surface.DrawRect((65 + hud_offset) * hud_scale, ScrH() - 64 * hud_scale - (hud_offset / 2), lenght * hud_scale, 6 * hud_scale)
 
         -- Foreground health
-        surface.SetDrawColor( rect_lightbg )
-        surface.DrawRect((71 + hud_offset) * hud_scale, ScrH() - 62 * hud_scale - (hud_offset / 2), pPlayer:Health() * 1.69 * hud_scale, 6 * hud_scale)
+        surface.SetDrawColor( rect_lightbg.r, rect_lightbg.g, rect_lightbg.b, light_background_alpha )
+        surface.DrawRect((65 + hud_offset) * hud_scale, ScrH() - 64 * hud_scale - (hud_offset / 2), pPlayer:Health() * 1.95 * hud_scale, 6 * hud_scale)
 
-    end )
+        --- Draw armorbar   
+        -- Background
+        surface.SetDrawColor( rect_darkbg.r, rect_darkbg.g, rect_darkbg.b, dark_background_alpha )
+        surface.DrawRect( (64 + hud_offset) * hud_scale, ScrH() - 78 * hud_scale - (hud_offset / 2), 63 * hud_scale, 10 * hud_scale )
+
+        surface.SetDrawColor( rect_darkbg.r, rect_darkbg.g, rect_darkbg.b, dark_background_alpha )
+        surface.DrawRect( (131 + hud_offset) * hud_scale, ScrH() - 78 * hud_scale - (hud_offset / 2), 63 * hud_scale, 10 * hud_scale )
+
+        surface.SetDrawColor( rect_darkbg.r, rect_darkbg.g, rect_darkbg.b, dark_background_alpha )
+        surface.DrawRect( (198 + hud_offset) * hud_scale, ScrH() - 78 * hud_scale - (hud_offset / 2), 63 * hud_scale, 10 * hud_scale )
+        
+        -- Armor chunks
+        surface.SetDrawColor( 0, 130, 255, 255 )
+        surface.SetMaterial( bgArmorGrain )
+        armor1Lenght = math.Clamp((pPlayer:Armor() / 4) * 7.63, 0, 63)
+        surface.DrawTexturedRectUV( (64 + hud_offset) * hud_scale, ScrH() - 78 * hud_scale - (hud_offset / 2), 0 + armor1Lenght * hud_scale, 10 * hud_scale, 0, 0, 63, 10 )
+
+        surface.SetDrawColor( 0, 130, 255, 255 )
+        surface.SetMaterial( bgArmorGrain )
+        armor2Lenght = math.Clamp((pPlayer:Armor() - 40) * 2.2, 0, 63)
+        surface.DrawTexturedRectUV( (131 + hud_scale) * hud_scale, ScrH() - 78 * hud_scale - (hud_offset / 2), 0 + armor2Lenght * hud_scale, 10 * hud_scale, 0, 0, 63, 10 )
+
+        surface.SetDrawColor( 0, 130, 255, 255 )
+        surface.SetMaterial( bgArmorGrain )
+        armor3Lenght = math.Clamp((pPlayer:Armor() - 70) * 2.2, 0, 63)
+        surface.DrawTexturedRectUV( (198 + hud_scale) * hud_scale, ScrH() - 78 * hud_scale - (hud_offset / 2), 0 + armor3Lenght * hud_scale, 10 * hud_scale, 0, 0, 63, 10 )
+
+        if (pPlayer:Armor() > 0 and pPlayer:Alive()) then
+            allow_armorbreak_effect = true
+        end
+
+        if pPlayer:Armor() == 0 and allow_armorbreak_effect then
+            surface.PlaySound("player/hit_armor_break_01.wav")
+            allow_armorbreak_effect = false            
+        end
+    end )  
 
     local pPlayer = LocalPlayer()   
 end
