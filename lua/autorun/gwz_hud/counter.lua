e3ab9390b1c1c24dc0428f0e62105d5ad451a54a
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2021-10-29 13:08:46 +0300

    Alive players in counter

--- lua/autorun/gwz_hud/counter.lua
+++ lua/autorun/gwz_hud/counter.lua
@@ -1,11 +1,16 @@
+aliveplayers = 0
+
 if CLIENT then
     local mGroups = Material("hud/counter_groups.png", "smooth")
     local mPlayers = Material("hud/counter_players.png", "smooth")
     local mkills = Material("hud/counter_kill.png", "smooth")
 
-    local aliveplayers = 0
-
     local enable = CreateClientConVar("gwz_hud_enable_counter", 1, true, true);
+    local numberofalive = 0
+
+    net.Receive("AlivePlayers", function()
+        numberofalive = net.ReadInt(32)
+    end)
 
     hook.Add( "HUDPaint", "GWZ_Counter", function()
         if !GetConVar("gwz_hud_enable"):GetBool() or !GetConVar("cl_drawhud"):GetBool() or !enable:GetBool() then return end
@@ -14,8 +19,6 @@ if CLIENT then
         if (game.SinglePlayer()) then return end
 
         surface.SetFont("GWZ_VerySmall")
-
-        aliveplayers = #player.GetAll()
 
         --- Number of kills
         -- Icon        
@@ -35,15 +38,39 @@ if CLIENT then
         surface.SetDrawColor(255,255,255,255)
         surface.SetMaterial(mPlayers)
         surface.DrawTexturedRect(ScrW() - 220, 50, 16, 22)
-        -- Number of players in server
-        -- TODO: num of alive players??
 
         surface.SetTextColor( 0, 0, 0 )
         surface.SetTextPos(ScrW() - 193, 46)
-        surface.DrawText(aliveplayers)
+        surface.DrawText(numberofalive)
 
         surface.SetTextColor( 255, 255, 255 )
         surface.SetTextPos(ScrW() - 195, 44)
-        surface.DrawText(aliveplayers)
+        surface.DrawText(numberofalive)
     end )
+end
+
+if SERVER then
+    util.AddNetworkString("AlivePlayers")
+
+    hook.Add("PlayerSpawn", "GWZ_AddAlivePlayers", function(ply)
+        aliveplayers = aliveplayers + 1
+        PrintMessage( HUD_PRINTTALK, aliveplayers )
+
+        net.Start("AlivePlayers")
+        net.WriteInt(aliveplayers, 32)
+        for _,player in pairs (player.GetAll()) do
+            net.Send(player)
+        end
+    end)
+
+    hook.Add("PlayerDeath", "GWZ_RemoveAlivePlayers", function(ply)
+        aliveplayers = aliveplayers - 1
+        PrintMessage( HUD_PRINTTALK, aliveplayers )
+
+        net.Start("AlivePlayers")
+        net.WriteInt(aliveplayers, 32)
+        for _,player in pairs (player.GetAll()) do
+            net.Send(player)
+        end
+    end)
 end
